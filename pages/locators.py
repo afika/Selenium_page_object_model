95ed9a3a6889f3a89ceade2905990c2f799d1c2c
afika <nastya.tk6990@gmail.com>
2019-07-12 03:58:27 +0700

    add test_can_add_product_to_cart

--- pages/locators.py
+++ pages/locators.py
@@ -7,3 +7,10 @@ class MainPageLocators(object):
 class LoginPageLocators(object):
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+class ProductPageLocators(object):
+    ADD_TO_BASKET_BUTTON = (By.CSS_SELECTOR, "#add_to_basket_form>button")
+    PRODUCT_NAME = (By.CSS_SELECTOR, ".content h1")
+    PRODUCT_PRICE = (By.CSS_SELECTOR,".content .price_color")
+    MESSAGE_ABOUT_ADDING = (By.CSS_SELECTOR,"#messages .alertinner")
+    MESSAGE_BASKET_TOTAL = (By.CSS_SELECTOR,"#messages .alert-info")
