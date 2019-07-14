bd496d5f96f1f1b8bfa2faa6f106a29180e184af
afika <nastya.tk6990@gmail.com>
2019-07-14 23:17:10 +0700

    add work with login page to BasePage

--- test_product_page.py
+++ test_product_page.py
@@ -1,8 +1,8 @@
 from .pages.product_page import ProductPage
+from .pages.login_page import LoginPage
 import time
 import pytest
 
-#@pytest.mark.skip
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
@@ -22,7 +22,6 @@ def  test_guest_can_add_product_to_cart(browser, link):
     product_page.should_be_message_about_adding()
     product_page.should_be_message_about_basket_total()
 
-
 def test_guest_cant_see_success_message_after_adding_product_to_cart(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
     product_page = ProductPage(browser, link)
@@ -30,7 +29,6 @@ def test_guest_cant_see_success_message_after_adding_product_to_cart(browser):
     product_page.add_product_to_basket()
     product_page.solve_quiz_and_get_code()
     product_page.should_not_be_success_message()
-
 
 def test_guest_cant_see_success_message(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
@@ -46,4 +44,17 @@ def test_message_disappeared_after_adding_product_to_cart(browser):
     product_page.solve_quiz_and_get_code()
     product_page.should_be_disappeared_success_message()
 
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+def test_guest_can_go_to_login_page_from_product_page (browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
 
